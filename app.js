55f2bd95caf13449f15a773ea8efaba7f6e76e71
Javier Peluso <jpelusocoderhouse@gmail.com>
2022-11-09 10:36:14 -0300

    update podedex

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000 //eslint-disable-line
+
+app.use(express.static('dist'))
+
+app.listen(PORT, () => {
+  console.log("server started on port 5000"); //eslint-disable-line
+})
